c5416a7998b030409afc717d16521224dac96d36
nico760 <84546260+nico760@users.noreply.github.com>
2023-05-13 13:52:10 -0500

    Update aspect_ratio_ex.dart

--- aspect_ratio_ex.dart
+++ aspect_ratio_ex.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: AsRatWid(),
+    )
+    );
+  }
+}
+
 class AsRatWid extends StatelessWidget {
   const AsRatWid({super.key});
-
-
   
   @override
   Widget build(BuildContext context) {
